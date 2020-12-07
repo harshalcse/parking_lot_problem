b517c6b3031ef4b3ac8aee0c3c9af5b9297ddc80
Harshal Bhamare <harshalcse2910@gmail.com>
2020-12-07 10:01:35 +0530

    files added for actual parking logic

---
+++ test/run.testcases.js
@@ -0,0 +1,5 @@
+require('./createparkslot.test.js');
+require('./park.test.js');
+require('./leavepark.test.js');
+require('./parkingstatus.test.js');
+require('./slot_number_for_registration_number.test.js');
